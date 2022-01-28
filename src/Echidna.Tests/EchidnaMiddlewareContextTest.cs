a382796fff6c1ecdcd2457f8327616af71475c99
Michael Adelson <mike.adelson314@gmail.com>
2022-01-28 15:23:52 -0500

    Add middleware structure

---
+++ src/Echidna.Tests/EchidnaMiddlewareContextTest.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Medallion.Data.Tests;
+
+internal class EchidnaMiddlewareContextTest
+{
+    [Test]
+    public void TestPipeline()
+    {
+        var middleware = new List<Func<EchidnaMiddlewareContext, ValueTask<object?>>>();
+        AddBranchingMiddleware(2);
+        AddBranchingMiddleware(3);
+        AddBranchingMiddleware(4);
+
+        var context = new InternalContext(middleware);
+        var task = context.ExecutePipelineAsync();
+        Assert.IsTrue(task.IsCompleted);
+        Assert.IsTrue(task.IsCompletedSuccessfully);
+        Assert.AreEqual(2 * 3 * 4, task.Result);
+
+        void AddBranchingMiddleware(int count) =>
+            middleware.Add(async c =>
+            {
+                for (var i = 0; i < count - 1; ++i) { await c.NextAsync(); }
+                return await c.NextAsync();
+            });
+    }
+
+    [Test]
+    public void TestCanPassDataBetweenMiddleware()
+    {
+        var middleware = new List<Func<EchidnaMiddlewareContext, ValueTask<object?>>>();
+        middleware.Add(async c =>
+        {
+            c.Data.Add("key1", 5);
+            await c.NextAsync();
+            return c.Data["key2"];
+        });
+        middleware.Add(async c =>
+        {
+            c.Data.Add("key2", 2 * (int)c.Data["key1"]!);
+            return await c.NextAsync();
+        });
+
+        var context = new InternalContext(middleware);
+        var task = context.ExecutePipelineAsync();
+        Assert.IsTrue(task.IsCompleted && task.IsCompletedSuccessfully);
+        Assert.AreEqual(10, task.Result);
+    }
+
+    [Test]
+    public async Task TestRetry()
+    {
+        var middleware = new List<Func<EchidnaMiddlewareContext, ValueTask<object?>>>();
+        middleware.Add(async c =>
+        {
+            var retries = 0;
+            while (true)
+            {
+                try { return await c.NextAsync(); }
+                catch when (++retries <= 3) { }
+            }
+        });
+
+        var context = new InternalContext(middleware) { ShouldThrow = c => c < 4 };
+        Assert.AreEqual(4, await context.ExecutePipelineAsync());
+
+        context = new InternalContext(middleware) { ShouldThrow = c => c < 5 };
+        Assert.ThrowsAsync<TimeZoneNotFoundException>(() => context.ExecutePipelineAsync().AsTask());
+    }
+
+    private class InternalContext : InternalMiddlewareContext
+    {
+        private int _executeCount;
+
+        public Func<int, bool> ShouldThrow { get; set; } = _ => false;
+
+        public InternalContext(IReadOnlyList<Func<EchidnaMiddlewareContext, ValueTask<object?>>> middleware)
+            : base(middleware, new SqlCommand(), isAsync: false)
+        {
+        }
+
+        protected override ValueTask<object?> InternalExecuteAsync()
+        {
+            var result = ++this._executeCount;
+            return this.ShouldThrow(result) ? throw new TimeZoneNotFoundException() : new(result);
+        }
+    }
+}
---
+++ src/Echidna/EchidnaMiddlewareContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Medallion.Data;
+
+internal abstract class InternalMiddlewareContext 
+{
+    private readonly IReadOnlyList<Func<EchidnaMiddlewareContext, ValueTask<object?>>> _middleware;
+    private readonly OneOf<DbCommand, DbBatch> _command;
+    private Dictionary<object, object?>? _data;
+
+    protected InternalMiddlewareContext(
+        IReadOnlyList<Func<EchidnaMiddlewareContext, ValueTask<object?>>> middleware,
+        OneOf<DbCommand, DbBatch> command, 
+        bool isAsync)
+    {
+        this._middleware = middleware;
+        this._command = command;
+        this.IsAsync = IsAsync;
+    }
+
+    public DbCommand? Command => this._command.First;
+    public DbBatch? Batch => this._command.Second;
+    public bool IsAsync { get; }
+
+    public IDictionary<object, object?> Data => this._data ??= new();
+
+    protected abstract ValueTask<object?> InternalExecuteAsync();
+
+    public ValueTask<object?> NextAsync(int index)
+    {
+        var middleware = this._middleware;
+        return index < middleware.Count
+            ? middleware[index](new(this, index + 1))
+            : this.InternalExecuteAsync();
+    }
+
+    public ValueTask<object?> ExecutePipelineAsync() => this.NextAsync(0);
+}
+
+// todo move
+internal readonly struct OneOf<TEither, TOr> 
+    where TEither : class
+    where TOr : class
+{
+    private readonly object _value;
+
+    private OneOf(object value) { this._value = value; }
+
+    public TEither? First => this._value as TEither;
+    public TOr? Second => this._value as TOr;
+
+    public static implicit operator OneOf<TEither, TOr>(TEither value) => new(value);
+    public static implicit operator OneOf<TEither, TOr>(TOr value) => new(value);
+}
+
+public struct EchidnaMiddlewareContext
+{
+    private readonly InternalMiddlewareContext _context;
+    private int _index;
+
+    internal EchidnaMiddlewareContext(InternalMiddlewareContext context, int index) 
+    {
+        this._context = context;
+        this._index = index;
+    }
+
+    public DbCommand? Command => this._context.Command;
+    public DbBatch? Batch => this._context.Batch;
+    public bool IsAsync => this._context.IsAsync;
+    public IDictionary<object, object?> Data => this._context.Data;
+
+    public ValueTask<object?> NextAsync() => this._context.NextAsync(this._index);
+}
